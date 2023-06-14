139cb9afe3a7f56fde5299b6eeeaabfc5ee09669
Erzhigit Keneshbekov <erzhigit.lif3@Erzhigits-MacBook-Pro.local>
2023-06-14 21:07:15 +0600

    create modelUserModel

--- index.js
+++ index.js
@@ -11,27 +11,8 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.post('/auth/register', (req, res) => {
 
-app.post('/auth/login', (req, res) => {
-  console.log(req.body);
-
-  if (req.body.email === 'test@test.ru') {
-    const token = jwt.sign (
-      {
-        email: req.body.email,
-        fullName: 'Вая Пупкин',
-      },
-      'secret123',
-      );
-  }
-
-  res.json({
-    success: true,
-    token,
-  });
 });
 
 app.listen(4444, (err) => {
