fdd878314fc981ead1bd51d2e9380efa1994e4bd
Erzhigit Keneshbekov <erzhigit.lif3@Erzhigits-MacBook-Pro.local>
2023-06-15 12:23:40 +0600

    install express validator and connect it to the project

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import { registerValidation } from './validations/auth.js';
+import { validationResult } from 'express-validator';
 
 mongoose
   .connect('mongodb+srv://admin:wwwwww@cluster0.wxpw9zr.mongodb.net/?retryWrites=true&w=majority')
@@ -11,8 +13,15 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', (req, res) => {
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
 
+  res.json ({
+    successs: true,
+  });
 });
 
 app.listen(4444, (err) => {
