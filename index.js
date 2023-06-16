48eb6a81c85e5868a887dca57be665773f7c42a5
Erzhigit Keneshbekov <erzhigit.lif3@Erzhigits-MacBook-Pro.local>
2023-06-16 19:50:49 +0600

    connect to the database through the mongodb compass program

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import { registerValidation } from './validations/auth.js';
 import { validationResult } from 'express-validator';
 import UserModel from './models/User.js'
 
 mongoose
-  .connect('mongodb+srv://admin:wwwwww@cluster0.wxpw9zr.mongodb.net/?retryWrites=true&w=majority')
+  .connect('mongodb+srv://yourchupuzrik:12345qwerty@cluster0.olqe075.mongodb.net/blog?retryWrites=true&w=majority')
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
@@ -14,22 +15,25 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
 
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
   const doc = new UserModel({
     email: req.body.email,
-    fullNameame: req.body.fullName,
+    fullName: req.body.fullName,
     avatarUrl: req.body.avatarUrl,
     passwordHash: req.body.password,
   });
 
-  res.json ({
-    successs: true,
-  });
+  const user = await doc.save();
+  res.json(user);
 });
 
 app.listen(4444, (err) => {
