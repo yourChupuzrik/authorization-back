8bbebc0ba6c8ceb87b2e0dfa1534c1d442a76486
Erzhigit Keneshbekov <erzhigit.lif3@Erzhigits-MacBook-Pro.local>
2023-06-18 16:06:05 +0600

    Handling Errors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { validationResult } from 'express-validator';
 import UserModel from './models/User.js'
 
 mongoose
-  .connect('mongodb+srv://yourchupuzrik:12345qwerty@cluster0.olqe075.mongodb.net/blog?retryWrites=true&w=majority')
+  .connect('mongodb+srv://yourchupuzrik:qwerty12345@1.mszkjk1.mongodb.net/man?retryWrites=true&w=majority')
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
@@ -16,24 +16,43 @@ const app = express();
 app.use(express.json());
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      }, 'secret123',{
+        expiresIn: '30d',
+      },
+    );
+
+    res.json({
+      ...user,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось зарегистрироваться',
+    });
   }
-
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash: req.body.password,
-  });
-
-  const user = await doc.save();
-  res.json(user);
 });
 
 app.listen(4444, (err) => {
